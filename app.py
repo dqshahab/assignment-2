2e65017284c146147e77a59be15ae81b927fdb52
dqshahab <ridershahab56@gmail.com>
2023-05-16 17:10:27 +0530

    updated!!

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ b='shahab'
 if a==a[::-1]:
     print(a+ ' is a palindrome')
 else:
-    print(a+ 'is not a palindrome')
+    print(a+ ' is not a palindrome')
 if b==b[::-1]:
-    print(b+ 'is a palindrome')
+    print(b+ ' is a palindrome')
 else:
-    print(b+ 'is not a palindrome')
+    print(b+ ' is not a palindrome')
